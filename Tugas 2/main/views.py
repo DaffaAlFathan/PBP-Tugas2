6d7c5e0074c948530ada77026c58ff0a1bf9f8b0
DaffaAlFathan <daffaalfathanzaki@gmail.com>
2023-09-12 20:25:48 +0700

    Ganti nama model dan data

--- Tugas 2/main/views.py
+++ Tugas 2/main/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Daffa Al Fathan Zaki',
-        'class': 'PBP A'
+        'name': 'Kirigaya Kazuto',
+        'class': 'PBP A',
+        'amount': 1,
+        'description': "Test",
     }
 
     return render(request, "main.html", context)
