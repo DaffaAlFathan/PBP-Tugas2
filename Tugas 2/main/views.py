fc5dcc8985f13d3f7fcb8e54d8aee760276122e4
DaffaAlFathan <daffaalfathanzaki@gmail.com>
2023-09-13 09:40:49 +0700

    Tambah checklist

--- Tugas 2/main/views.py
+++ Tugas 2/main/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Kirigaya Kazuto',
+        'name': 'Daffa Al Fathan Zaki',
         'class': 'PBP A',
         'amount': 1,
         'description': "Test",
