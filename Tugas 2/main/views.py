0d1a80425dd0ffe7fe1e41636dcbdcfd60343acb
DaffaAlFathan <daffaalfathanzaki@gmail.com>
2023-09-20 10:56:53 +0700

    Tugas 3 1st

--- Tugas 2/main/views.py
+++ Tugas 2/main/views.py
@@ -1,13 +1,43 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect, HttpResponse
+from main.forms import ItemForm
+from django.urls import reverse
+from main.models import Item
+from django.core import serializers
 
 # Create your views here.
 def show_main(request):
+    items = Item.objects.all()
     context = {
         'name': 'Daffa Al Fathan Zaki',
         'class': 'PBP A',
-        'amount': 1,
-        'description': "Test",
+        'items': items,
     }
 
     return render(request, "main.html", context)
+
+def create_product(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "create_product.html", context)
+
+def show_xml(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
